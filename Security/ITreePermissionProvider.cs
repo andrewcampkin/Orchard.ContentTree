b137b9364ab872a65aeef6de229b50fb5852b785
Andrew Campkin <andrewcampkin@gmail.com>
2018-05-16 13:13:32 +1200

    Copied source from codeplex archive extract.  Fixed issue with redirect back from action.

---
+++ Security/ITreePermissionProvider.cs
@@ -0,0 +1,20 @@
+﻿namespace Orchard.ContentTree.Security {
+    using ContentManagement;
+    using Core.Contents;
+    using Orchard.Security;
+
+    public interface ITreePermissionProvider : IDependency {
+        bool Editable(IContent content);
+    }
+
+    public class DefaultTreePermissionProvider : ITreePermissionProvider {
+        private readonly IAuthorizer _authorizer;
+        public DefaultTreePermissionProvider(IAuthorizer authorizer) {
+            _authorizer = authorizer;
+        }
+
+        public bool Editable(IContent content) {
+            return _authorizer.Authorize(Permissions.EditContent, content);
+        }
+    }
+}
